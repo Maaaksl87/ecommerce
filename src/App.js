d52129b40366613d721de178bc0b3a8f6b49507a
Maaaksl87 <maaaksl87@gmail.com>
2024-04-02 21:21:20 +0300

    add react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <span>Home</span>
+  },
+  {
+    path: "/products/:id",
+    element: <span>Category</span>
+  },
+  {
+    path: "/product/:id",
+    element: <span>Product</span>
+  },
+]);
+
 function App() {
   return (
-    <div className="App">
-      Internet Shop "Avalanche"
+    <div>
+      <RouterProvider router={router} />
     </div>
   );
 }
