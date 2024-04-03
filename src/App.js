889da79e6b6ae78336d2ef7ed06fbfb71c3b257c
Maaaksl87 <maaaksl87@gmail.com>
2024-04-03 20:23:08 +0300

    add footer and navbar files

--- src/App.js
+++ src/App.js
@@ -1,31 +1,51 @@
 import {
   createBrowserRouter,
   RouterProvider,
+  Outlet
 } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import Products from "./pages/Products/products";
 import Product from "./pages/Product/product";
+import Navbar from "./components/Navbar/Navbar";
+import Footer from "./components/Footer/Footer";
+
+const Layout = () => {
+  return (
+    <div className="App" >
+      <Navbar />
+      <Outlet />
+      <Footer />
+    </div >
+  )
+}
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Home />
-  },
-  {
-    path: "/products/:id",
-    element: <Products />
-  },
-  {
-    path: "/product/:id",
-    element: <Product/>
-  },
-]);
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />
+      },
+      {
+        path: "/products/:id",
+        element: <Products />
+      },
+      {
+        path: "/product/:id",
+        element: <Product />
+      }
+    ]
+  }
+])
+
 
 function App() {
   return (
     <div>
       <RouterProvider router={router} />
-    </div>
+    </div >
   );
 }
 
