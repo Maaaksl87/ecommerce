0a1266a2437a5ae4d54bdcd7ddc89110b8603185
Maaaksl87 <maaaksl87@gmail.com>
2024-04-05 22:41:55 +0300

    зробив попередню версію navbar, добавив до нього стилі

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,8 +1,40 @@
-import React from "react"
+import React from "react";
+
+import SearchIcon from '@mui/icons-material/Search'; // іконка пошуку
+import PersonOutlineOutlinedIcon from '@mui/icons-material/PersonOutlineOutlined'; // іконка юзера
+import FavoriteBorderOutlinedIcon from '@mui/icons-material/FavoriteBorderOutlined'; // іконка серця
+import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined'; //іконка корзини
+import { Link } from "react-router-dom";
+import "./Navbar.scss";
+
 const Navbar = () => {
     return (
-        <div className="navbar">
-            
+        <div className="wrapper_nav">
+            <div className="navbar">
+                <div className="wrapper">
+
+                    <div className="left" >
+                        <Link to="/">Avalanche</Link>
+                    </div>
+                    <div className="right">
+                        <ul>
+                            <li>До дому</li>
+                            <li>Спорядження</li>
+                            <li>Про сайт</li>
+                            <li>Контакти</li>
+                        </ul>
+                        <div className="icons">
+                            <SearchIcon />
+                            <PersonOutlineOutlinedIcon />
+                            <FavoriteBorderOutlinedIcon />
+                            <div className="cartIcon">
+                                <ShoppingCartOutlinedIcon />
+                                <span>0</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
