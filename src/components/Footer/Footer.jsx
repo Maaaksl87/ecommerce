e2737b27faf3800f0e14afcc77d902795b59d551
Maaaksl87 <maaaksl87@gmail.com>
2024-04-09 15:56:31 +0300

    add footer structure

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,41 @@
-import React from "react"
+import React from "react";
+import { Link } from "react-router-dom";
+
+import InstagramIcon from '@mui/icons-material/Instagram'; // інстаграм
+import FacebookIcon from '@mui/icons-material/Facebook'; // фейсбук
+import MailOutlineIcon from '@mui/icons-material/MailOutline'; // емейл
+
+import "./Footer.scss"
 const Footer = () => {
     return (
-        <div>Footer</div>
+        <footer className="footer">
+            <div className="container">
+                <div className="footer__row">
+                    <div className="footer__col footer__logo">
+                        <Link to="/">Logo</Link>
+                    </div>
+                    <div className="footer__col">
+                        <h2 className="footer__title"></h2>
+                        <div className="footer__nav"></div>
+                    </div>
+                    <div className="footer__col">
+                        <h2 className="footer__title"></h2>
+                        <div className="footer__nav"></div>
+                    </div>
+                    <div className="footer__col">
+                        <h2 className="footer__subscribe"></h2>
+                        <div className="footer__title"></div>
+                    </div>
+                    <ul className="icons">
+                        <InstagramIcon />
+                        <FacebookIcon />
+                        <MailOutlineIcon />
+                    </ul>
+                </div>
+
+                <div className="footer__bottom"></div>
+            </div>
+        </footer>
     )
 }
 
