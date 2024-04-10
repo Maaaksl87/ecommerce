93603467e59cd6abca6961396bdca7e9df671015
Maaaksl87 <maaaksl87@gmail.com>
2024-04-10 16:58:03 +0300

    попередня версія footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -6,34 +6,37 @@ import FacebookIcon from '@mui/icons-material/Facebook'; // фейсбук
 import MailOutlineIcon from '@mui/icons-material/MailOutline'; // емейл
 
 import "./Footer.scss"
+
 const Footer = () => {
     return (
         <footer className="footer">
-            <div className="container">
+            <div className="footer__container container">
                 <div className="footer__row">
                     <div className="footer__col footer__logo">
                         <Link to="/">Logo</Link>
                     </div>
                     <div className="footer__col">
-                        <h2 className="footer__title"></h2>
-                        <div className="footer__nav"></div>
+                        <h2 className="footer__title">Магазин</h2>
+                        <div className="footer__nav">
+                            <ul>
+                                <li><a href="/">Про магазин</a></li>
+                                <li><a href="/">Товари</a></li>
+                                <li><a href="/">Спорядження</a></li>
+                                <li><a href="/">Більше</a></li>
+                            </ul>
+                        </div>
                     </div>
                     <div className="footer__col">
-                        <h2 className="footer__title"></h2>
-                        <div className="footer__nav"></div>
+                        <h2 className="footer__title">Зв'язок з нами</h2>
+                        <ul className="icons">
+                            <InstagramIcon />
+                            <FacebookIcon />
+                            <MailOutlineIcon />
+                        </ul>
                     </div>
-                    <div className="footer__col">
-                        <h2 className="footer__subscribe"></h2>
-                        <div className="footer__title"></div>
-                    </div>
-                    <ul className="icons">
-                        <InstagramIcon />
-                        <FacebookIcon />
-                        <MailOutlineIcon />
-                    </ul>
                 </div>
 
-                <div className="footer__bottom"></div>
+                <div className="footer__bottom">Copyright</div>
             </div>
         </footer>
     )
