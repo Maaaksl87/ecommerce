d27d45b55f692292989140316cff9bde7a595165
Maaaksl87 <maaaksl87@gmail.com>
2024-05-30 01:24:42 +0300

    Header 85%, footer 70%

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,45 +1,74 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-import InstagramIcon from '@mui/icons-material/Instagram'; // інстаграм
-import FacebookIcon from '@mui/icons-material/Facebook'; // фейсбук
-import MailOutlineIcon from '@mui/icons-material/MailOutline'; // емейл
+import InstagramIcon from "@mui/icons-material/Instagram"; // інстаграм
+import FacebookIcon from "@mui/icons-material/Facebook"; // фейсбук
+import MailOutlineIcon from "@mui/icons-material/MailOutline"; // емейл
 
-import "./Footer.scss"
+import "./Footer.scss";
 
 const Footer = () => {
-    return (
-        <footer className="footer">
-            <div className="footer__container container">
-                <div className="footer__row">
-                    <div className="footer__col footer__logo">
-                        <Link to="/">Logo</Link>
-                    </div>
-                    <div className="footer__col">
-                        <h2 className="footer__title">Магазин</h2>
-                        <div className="footer__nav">
-                            <ul>
-                                <li><a href="/">Про магазин</a></li>
-                                <li><a href="/">Товари</a></li>
-                                <li><a href="/">Спорядження</a></li>
-                                <li><a href="/">Більше</a></li>
-                            </ul>
-                        </div>
-                    </div>
-                    <div className="footer__col">
-                        <h2 className="footer__title">Зв'язок з нами</h2>
-                        <ul className="icons">
-                            <InstagramIcon />
-                            <FacebookIcon />
-                            <MailOutlineIcon />
-                        </ul>
-                    </div>
-                </div>
-
-                <div className="footer__bottom">Copyright</div>
+  return (
+    <footer className="footer">
+      <div className="footer__container">
+        <div className="footer__row">
+          <div className="footer__col footer__logo">
+            <Link to="/">Logo</Link>
+          </div>
+          <div className="footer__col">
+            <h2 className="footer__title">Магазин</h2>
+            <div className="footer__nav">
+              <ul>
+                <li>
+                  <Link to="/about">Про магазин</Link>
+                </li>
+                <li>
+                  <Link to="/products">Товари</Link>
+                </li>
+                <li>
+                  <Link to="/equipment">Спорядження</Link>
+                </li>
+                <li>
+                  <Link to="/more">Більше</Link>
+                </li>
+              </ul>
             </div>
-        </footer>
-    )
-}
+          </div>
+          <div className="footer__col">
+            <h2 className="footer__title">Зв'язок з нами</h2>
+            <ul className="icons">
+              <li>
+                <a
+                  href="https://www.instagram.com"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <InstagramIcon />
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://www.facebook.com"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <FacebookIcon />
+                </a>
+              </li>
+              <li>
+                <a href="mailto:example@example.com">
+                  <MailOutlineIcon />
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className="footer__bottom">
+          © 2024 Avalanche. All rights reserved.
+        </div>
+      </div>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,8 +1,97 @@
-import React from "react"
+import React from "react";
 const Home = () => {
-    return (
-        <div>Home</div>
-    )
-}
+  return (
+    <div>
+      nctio nobis sequi ratione accusamus impedit vero consectetur et numquam
+      architecto quibusdam pariatur aliquid id, mosquam fugiat impedit autem
+      fugit repellat tempora quam dolor deserunt quaerat est atque, quas, sed
+      nemo maxime ducimus, dolorem esse quos dolores reprehenderit non totam
+      porro voluptas voluptatum expedita! Dolore saepe nulla laudantium optio
+      consequatur libero, amet, alias adipisci quod nihil eum consequuntur?
+      Quidem, cumque minima. Laborum molestias accusantium expedita rerum.
+      Perspiciatis qui eveniet re culpa excepturi molestias. In reprehenderit
+      voluptates aperiam, deleniti placeat quisquam dicta, porro alias
+      temporibus voluptatum ab nulla voluptatem at, sequi perspiciatis quasi
+      perferendis possimus eos. Vitae expedita repellat ab pariatur labore aut
+      omnis sunt harum, ad, amet ducimus nisi autem facere quasi corporis sed
+      velit, minus aliquam voluptatibus placeat et nulla nemo voluptatum.
+      Itaque, placeat ullam rerum culpa nemo animi, atque voluptas voluptate
+      quis veritatis obcaecati debitis! Ad, aliquid id. Repellendus porro ipsum
+      suscipit, earum sunt iure, esse aspernatur deserunt eos adipisci laborum
+      odit vitae quidem, voluptates quos magni dignissimos quis nulla
+      perferendis. Optio cupiditate dolor porro ipsum dolores quidem saepe
+      repellendus quaerat et aliquam cum fugit suscipit unde, quae laboriosam
+      tenetur, dolore, earum tempore eum fuga! Quidem fugiat deleniti nobis
+      obcaecati, odit nostrum assumenda alias, libero aperiam, dicta modi illo
+      eos omnis cumque incidunt quas tempora rerum fugit minus! Laborum nisi,
+      vel illum numquam, nostrum ipsam necessitatibus dolores perferendis
+      ducimus voluptatem voluptates, corrupti veritatis placeat error tempora
+      tempore! Placeat doloremque ad, blanditiis commodi recusandae iusto,
+      soluta assumenda ut excepturi veniam voluptatem? Ipsum, repellat deserunt.
+      Dolor illo rerum in est. Quidem pariatur fuga hic aperiam necessitatibus
+      dignissimos corporis impedit similique, est architecto cumque eius
+      reprehenderit, magni amet saepe ea accusamus? Commodi dolore vel
+      recusandae! Hic, eos? Possimus doloribus eos incidunt ipsam harum quam
+      assumenda. Magnam ab minima impedit sit perspiciatis, deleniti voluptates
+      hic, asperiores expedita provident quidem, mollitia repudiandae
+      consequatur quae? Impedit quis, recusandae hic odit, laboriosam, odio
+      consectetur quam necessitatibus modi cum eligendi blanditiis dignissimos
+      officiis accusamus. Laboriosam quidem quis officia aliquam, aperiam
+      nesciunt illo quo? Velit error minima numquam perferendis, laboriosam odit
+      voluptas aperiam voluptatem incidunt ducimus reiciendis sequi voluptatibus
+      dicta. Ipsum ea dignissimos sit molestias quos harum, explicabo sunt
+      aspernatur dolorem cumque, reiciendis delectus praesentium accusamus
+      incidunt vel velit similique distinctio rem voluptatum ullam dolores eum,
+      quam nam. Officia quam pariatur nemo inventore. Animi repellat
+      voluptatibus perferendis, cupiditate qui nemo esse fugit aspernatur ea
+      vitae est architecto quasi tempore ad velit ut eveniet, eius
+      exercitationem expedita natus omnis. Dolore esse provident, in molestiae
+      voluptate natus perspiciatis veritatis impedit. Vero laudantium expedita
+      aspernatur cupiditate repudiandae aliquid. Quibusdam officiis architecto
+      enim consequuntur sit quaerat, quae temporibus, reprehenderit eius totam
+      nam sequi accusantium aut quis doloremque saepe repellendus itaque modi,
+      beatae quasi id doloribus obcaecati? Quaerat cum praesentium pariatur amet
+      eius iusto. Deserunt possimus exercitationem, voluptatibus assumenda iure
+      temporibus nisi hic qui ea culpa a accusamus alias molestiae quidem
+      necessitatibus consequatur itaque laudantium totam ratione aliquam laborum
+      atque? Ducimus voluptatibus praesentium eaque, odio illum asperiores.
+      Consequuntur nulla, magni eligendi necessitatibus voluptates, magnam ex,
+      excepturi doloribus quos accusantium inventore? Nostrum ipsa ullam
+      accusantium, modi vero suscipit rerum accusamus maxime laborum aut
+      molestiae dolore quas iste alias totam aperiam repellat assumenda placeat
+      maiores minus eligendi necessitatibus nihil optio? Assumenda, dolores
+      impedit! Nam explicabo maiores dolor repellat nemo earum, illo vero ipsum
+      eos cupiditate aspernatur sed quas repudiandae cum laboriosam vel nobis
+      id! Magni in nemo iste quis ea veritatis officia laboriosam molestias,
+      voluptates eveniet reprehenderit inventore fugit explicabo obcaecati,
+      quidem necessitatibus commodi, recusandae non totam rem mollitia saepe
+      autem dolorem possimus? Est suscipit a, officia eaque sunt labore repellat
+      tempore, minus voluptates nobis id hic enim provident laboriosam
+      reprehenderit obcaecati at vitae perferendis totam facilis expedita eius?
+      Non, modi atque iure reiciendis ut nam. Cumque eum, obcaecati id saepe
+      aspernatur dignissimos placeat, rem maiores impedit recusandae
+      reprehenderit dolorem delectus aliquam fugiat odit. Culpa itaque,
+      perspiciatis distinctio corporis aliquam velit impedit ipsum expedita,
+      accusamus dolor non natus exercitationem suscipit ea nihil officia rem
+      quasi sit. Consequuntur quisquam reiciendis, quas libero quasi autem aut
+      quidem dolorum optio doloremque! Dolor magnam est distinctio optio quae in
+      eos. Dicta exercitationem repudiandae eveniet fuga. Maxime recusandae
+      blanditiis illum possimus corporis voluptatibus dolorem impedit,
+      reprehenderit saepe quod qui neque omnis hic odio nesciunt quos, facere
+      vitae dolorum quisquam architecto, deleniti doloremque mollitia. Placeat
+      error ipsum veniam ratione maiores aspernatur, quod pariatur minus
+      architecto quis nisi rem accusantium expedita assumenda dolorum quia
+      provident illum magni eum eveniet soluta. Dicta, nulla numquam asperiores
+      porro blanditiis voluptatibus quia voluptate inventore autem maiores
+      itaque, cumque optio ad quis corrupti accusamus quod saepe eveniet impedit
+      necessitatibus repellendus culpa. Officiis, ea. Eius voluptatum beatae qui
+      molestias molestiae facilis voluptate doloremque repellat veritatis
+      quisquam ullam quasi minima non architecto illum dolore, officia natus
+      cupiditate dolor similique harum deleniti quam! Aliquid rem laudantium
+      aperiam similique quae blanditiis, molestias harum cumque, ullam
+      repellendus possimus. Modi veniam dicta unde dolorem sed.
+    </div>
+  );
+};
 
-export default Home
+export default Home;
