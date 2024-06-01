26d49cb4629538493f53b72eebfa6a15fb2dc4a7
Maaaksl87 <maaaksl87@gmail.com>
2024-06-01 03:38:11 +0300

    сортування ціни та карток успішна спроба №1

--- src/pages/Products/products.jsx
+++ src/pages/Products/products.jsx
@@ -1,8 +1,64 @@
-import React from "react"
-const Products = () => {
-    return (
-        <div>Products</div>
-    )
-}
+import React from "react";
+import { useState } from "react";
+import { useParams } from "react-router-dom";
+import List from "../../components/List/List";
+import "./Products.scss";
 
-export default Products
+const Products = () => {
+  const catId = parseInt(useParams().id);
+  const [maxPrice, setMaxPrice] = useState(1000);
+  const [sort, setSort] = useState(null);
+
+  return (
+    <div className="products">
+      <div className="left">
+        <div className="filterItem">
+          <h2>Filter by price</h2>
+          <div className="inputItem">
+            <span>0</span>
+            <input
+              type="range"
+              min={0}
+              max={1000}
+              onChange={(e) => setMaxPrice(e.target.value)}
+            />
+            <span>{maxPrice}</span>
+          </div>
+        </div>
+        <div className="filterItem">
+          <h2>Сортувати за</h2>
+          <div className="inputItem">
+            <input
+              type="radio"
+              id="asc"
+              value="asc"
+              name="price"
+              onChange={(e) => setSort("asc")}
+            />
+            <label htmlFor="asc">Спочатку нижча</label>
+          </div>
+          <div className="inputItem">
+            <input
+              type="radio"
+              id="desc"
+              value="desc"
+              name="price"
+              onChange={(e) => setSort("desc")}
+            />
+            <label htmlFor="desc">Спочатку вища</label>
+          </div>
+        </div>
+      </div>
+
+      <div className="right">
+        <img
+          src="https://plus.unsplash.com/premium_photo-1683850244158-4eff80f5c925?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8YWxwaW5pc218ZW58MHx8MHx8fDA%3D"
+          alt="alpinism photo"
+        />
+        <List />
+      </div>
+    </div>
+  );
+};
+
+export default Products;
